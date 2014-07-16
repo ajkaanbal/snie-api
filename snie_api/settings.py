1fa13ffe2d815a0b24de83458865984a0c723b28
Ricardo M. Vilchis <ajkaanbal@gmail.com>
2014-07-15 21:56:17 -0500

    in the begining, darkness was over the surface of the deep...

---
+++ snie_api/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for snie_api project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'snie_api'
+
+SPIDER_MODULES = ['snie_api.spiders']
+NEWSPIDER_MODULE = 'snie_api.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'snie_api (+http://www.yourdomain.com)'
